201898a4fa4be844114f6a29b1f1b44085b422cb
sofiiagran <sofiiagran@gmail.com>
2025-11-20 21:55:34 +0100

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Slay <code>src/App.js</code> and save to reload.
         </p>
       </header>
     </div>
